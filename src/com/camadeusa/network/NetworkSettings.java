057d8ab495581ce66e7bb5524a5dee447d8bf6d0
CAmadeusA <CAmadeusA@users.noreply.github.com>
2017-10-25 20:50:45 -0500

    Another conflict found lol

--- src/com/camadeusa/network/NetworkSettings.java
+++ src/com/camadeusa/network/NetworkSettings.java
@@ -1,4 +1,4 @@
-package com.camadeusa.module.network;
+package com.camadeusa.network;
 
 import java.util.HashMap;
 
@@ -14,23 +14,24 @@ import com.rethinkdb.net.Cursor;
 import net.ME1312.SubServers.Client.Bukkit.SubAPI;
 
 public class NetworkSettings {
-	
+
 	HashMap<String, JSONObject> settings = new HashMap<>();
 
 	public NetworkSettings() {
-		
+
 		SubAPI.getInstance().getSubDataNetwork().sendPacket(new PacketDownloadNetworkSettings(callback -> {
 			for (String key : callback.getJSONObject("data").keySet()) {
 				settings.put(key, callback.getJSONObject("data").getJSONObject(key));
 			}
 		}));
-		
+
 		Bukkit.getScheduler().runTaskAsynchronously(NetworkCore.getInstance(), new Runnable() {
 			@Override
 			public void run() {
-				Connection con = RethinkDB.r.connection().hostname("192.168.1.100").db("Orion_Network").user("admin", "61797Caa").connect();
+				Connection con = RethinkDB.r.connection().hostname("192.168.1.100").db("Orion_Network")
+						.user("admin", "61797Caa").connect();
 				con.use("Orion_Network");
-				
+
 				Cursor<JSONObject> cur = RethinkDB.r.db("Orion_Network").table("networksettings").changes().run(con);
 				for (JSONObject change : cur) {
 					if (settings.containsKey(change.getString("id"))) {
@@ -39,8 +40,8 @@ public class NetworkSettings {
 						settings.put(change.getString("id"), change);
 					}
 				}
-				
+
 			}
 		});
-	}	
+	}
 }
